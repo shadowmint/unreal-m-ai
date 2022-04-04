cbb764f56405eebbc98899a31f91615f768c3a0e
Doug <douglas.linder@gmail.com>
2022-04-04 17:46:32 +0800

    initial import

---
+++ src/MAi/Source/TemplateSamples/Private/TemplateBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TemplateBPLibrary.h"
+#include "Template.h"
+
+UTemplateBPLibrary::UTemplateBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UTemplateBPLibrary::TemplateSampleFunction(float Param)
+{
+	return -1;
+}
+
